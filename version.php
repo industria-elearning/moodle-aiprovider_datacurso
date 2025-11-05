8c3ae9e2688bb12820335ec73006d770c7ff486e
wilber-buendata <wilber@buendata.com>
2025-11-04 20:02:13 -0500

    chore: bump version to 2025110400

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'aiprovider_datacurso';
 $plugin->release = '1.0.2';
-$plugin->version = 2025103002;
+$plugin->version = 2025110400;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
